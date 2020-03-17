6c076318a3fd2cae8b4b98c9ff17abeb8ce22616
cloudmill <info@cloudmill.ru>
2020-03-17 11:16:20 +0300

    полный перенос на модули, обновление темплейта до новой версии

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// webpack.config.js
+module.exports = {
+  output: {
+    filename: "scripts.min.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
