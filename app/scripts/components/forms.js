fc197197c23dbca862b0a10c0883e79e13705c67
Дмитрий Антонов <dma@cloudmill.ru>
2020-04-02 15:52:49 +0300

    [UPDATE/ADD] новая страница "Где купить"
    без адаптива и логи для карты

---
+++ app/scripts/components/forms.js
@@ -0,0 +1,20 @@
+import $ from "jquery";
+
+import select2 from 'select2';
+
+let forms = {
+  init: function() {
+    //select2($);
+    this.select2Init();
+  },
+  select2Init: function() {
+    $(".select2-field").each((key,item)=>{
+      $(item).select2({
+        language: "ru",
+        dropdownParent: $(item).parent()
+      })
+    })
+  }
+};
+
+export default forms;
