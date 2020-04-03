4e9585d4e004d1a0a3f433c3d9eae98c8ef446a6
Дмитрий Антонов <dma@cloudmill.ru>
2020-04-03 12:55:59 +0300

    [UPDATE] Логика для карты на странице "Где купить"

--- app/scripts/components/maps.js
+++ app/scripts/components/maps.js
@@ -1,99 +1,162 @@
 import $ from "jquery";
 import ymaps from "ymaps";
+import Scrollbar from "smooth-scrollbar";
 
+const linkYmaps =
+  "https://api-maps.yandex.ru/2.1/?apikey=587b64ae-7249-4473-827f-ce90ff9529de&lang=ru_RU";
+
+let Ymaps;
 let maps = {
   init: function() {
-    console.log('fff')
-    ymaps.load(
-      "https://api-maps.yandex.ru/2.1/?apikey=587b64ae-7249-4473-827f-ce90ff9529de&lang=ru_RU"
-    ).then(maps => {
-      
+    ymaps.load(linkYmaps).then(maps => {
       if ($("#map").length > 0) {
-        
-        this.contactsMap.init(maps);
+        Ymaps = window.Ymaps = maps;
+        this.setup();
       }
     });
   },
+  setup: function() {
+    if ($(".whereBuy-map").length > 0) {
+      this.whereBuyMap.init();
+    } else if ($(".contacts-map")) {
+      this.contactsMap.init();
+    }
+  },
   contactsMap: {
-    center: null,
-    createPlaceMark: function(coords, src, sity, name, link) {
-      return new this.maps.Placemark(
-        coords,
-        {
-          hasBalloon: false
-        },
-        {
-          hideIconOnBalloonOpen: false,
-          iconLayout: "default#image",
-          iconImageHref: "images/placeMarkMap.png",
-          iconImageSize: [90, 87],
-          iconImageOffset: [-45, -44]
-        }
-      );
-    },
-    create: function() {
-      var _this = this;
-      var myMap = new this.maps.Map(
-        "map",
-        {
-          center: _this.center,
-          zoom: 6,
-          controls: ['smallMapDefaultSet']
-        },
-        {
-          searchControlProvider: "yandex#search"
-        }
-      );
-      var clusterer = new this.maps.Clusterer({
-        preset: "islands#invertedVioletClusterIcons",
-        clusterIcons: [
-          {
-            href: "images/placeMarkMap.png",
-            size: [90, 87],
-            offset: [-45, -44]
-          }
-        ],
-        groupByCoordinates: false,
-        clusterIconColor: "black",
-        clusterHideIconOnBalloonOpen: false,
-        geoObjectHideIconOnBalloonOpen: false
+    init: function() {
+      let map = new Map("contacts");
+    }
+  },
+  whereBuyMap: {
+    logic: function() {
+      $("#setGeoInMap").click(() => {
+        let btnGeo = $("[class*=-float-button-icon_icon_geolocation]");
+        if (btnGeo.length > 0) btnGeo.click();
       });
-      if ($(".map_data .placeMark").length > 0) {
-        $(".map_data .placeMark").each(function() {
-          clusterer.add(
-            _this.createPlaceMark(
-              $(this)
-                .attr("data-cords")
-                .split(","),
-              $(this).attr("data-name")
-            )
-          );
-        });
-      }
-      myMap.geoObjects.add(clusterer);
-      //if ($("#map").hasClass("hasBalloon"))
-      myMap.geoObjects.options.set({ hasBalloon: false });
-      // if ($(window).width() < 768) {
-      //   // myMap.behaviors.disable("drag");
-      // }
-      myMap.events.add("click", function() {
-        myMap.balloon.close();
-      });
-      myMap.behaviors.disable("scrollZoom");
     },
-    init: function(maps) {
-      this.maps = maps;
-      var _this = this;
-      this.center =
-        $(".map_data .placeMark").length > 0
-          ? $(".map_data .placeMark")
-              .eq(0)
-              .attr("data-cords")
-              .split(",")
-          : [55.751574, 37.573856];
-      this.create();
+    init: function() {
+      let map = new Map("whereBuy");
+      this.logic();
     }
   }
 };
+
+class Map {
+  constructor(type) {
+    this.type = type;
+    ymaps.load(linkYmaps).then(maps => {
+      if ($("#map").length > 0) {
+        this.Ymaps = maps;
+        this.init();
+      }
+    });
+  }
+  init() {
+    /* Создание карты */
+    this.center = [44.86402, 40.073503];
+    this.map = new this.Ymaps.Map(
+      "map",
+      {
+        center: this.center,
+        zoom: 15,
+        controls: ["smallMapDefaultSet"]
+      },
+      {
+        searchControlProvider: "yandex#search"
+      }
+    );
+
+    /* создание кластера для меток */
+    this.clusterer = new this.Ymaps.Clusterer({
+      preset: "islands#invertedVioletClusterIcons",
+      clusterIcons: [
+        {
+          href: "images/placeMarkMap.png",
+          size: [90, 87],
+          offset: [-45, -44]
+        }
+      ],
+      groupByCoordinates: false,
+      clusterIconColor: "black",
+      clusterHideIconOnBalloonOpen: false,
+      geoObjectHideIconOnBalloonOpen: false
+    });
+
+    /* Заполнение карты метками */
+    this.setPlacemarks();
+
+    this.map.geoObjects.add(this.clusterer);
+    this.map.geoObjects.options.set({ hasBalloon: false });
+
+    this.map.events.add("click", () => {
+      this.map.balloon.close();
+    });
+    this.map.behaviors.disable("scrollZoom");
+    if (this.clusterer._objectsCounter > 1)
+      this.map.setBounds(this.map.geoObjects.getBounds());
+  }
+  setPlacemarks() {
+    if ($(".map_data .placeMark").length > 0) {
+      $(".map_data .placeMark").each((key, item) => {
+        let coords = $(item)
+            .attr("data-cords")
+            .split(","),
+          name = $(item).attr("data-name"),
+          id = $(item).attr("data-id");
+
+        this.clusterer.add(this.createPlaceMark(coords, name, id));
+      });
+    }
+  }
+  createPlaceMark(coords, name, id) {
+    let placeMark = new this.Ymaps.Placemark(
+      coords,
+      {
+        hasBalloon: false,
+        hintContent: name
+      },
+      {
+        hideIconOnBalloonOpen: false,
+        iconLayout: "default#image",
+        iconImageHref: "images/placeMarkMap.png",
+        iconImageSize: [90, 87],
+        iconImageOffset: [-45, -44]
+      }
+    );
+    placeMark.id = id;
+    placeMark.name = name;
+
+    if (this.type == "whereBuy") {
+      this.linkedWithPointForWhereBuy(placeMark);
+    }
+
+    return placeMark;
+  }
+  linkedWithPointForWhereBuy(placeMark) {
+    let pointTarget = $(".whereBuy-map-point[data-id=" + placeMark.id + "]");
+
+    pointTarget.click(() => {
+      placeMark.events.fire("click", {
+        coordPosition: placeMark.geometry.getCoordinates(),
+        target: placeMark
+      });
+      this.map.panTo([placeMark.geometry.getCoordinates()], {
+        flying: true
+      });
+    });
+    placeMark.events.add("click", e => {
+      //Активация элелмента в списке и проктутка до него
+      var tempScrollbar = Scrollbar.get($(".whereBuy-map-points").eq(0)[0]);
+      tempScrollbar.scrollIntoView(pointTarget.eq(0)[0], {
+        offsetTop: 12,
+        offsetLeft: 34,
+        onlyScrollIfNeeded: true
+      });
+
+      $(".whereBuy-map-point").removeClass("active");
+      pointTarget.addClass("active");
+    });
+  }
+}
 
 export default maps;
