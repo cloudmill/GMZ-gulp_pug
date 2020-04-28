e42aba52b1eba61f5a31681c7d9be73e273bc057
Дмитрий Антонов <dma@cloudmill.ru>
2020-04-28 13:18:33 +0300

    [UPDATE] рефокторинг контроллера

--- app/scripts/animations/controllerElems/scene.js
+++ app/scripts/animations/controllerElems/scene.js
@@ -1,51 +1,14 @@
 import $ from "jquery";
-import Updater from "./updater.js";
+import updateController from "./updateController.js";
 import conf from "./conf.js";
 
-export default class Scene {
+export default class Scene extends updateController {
   constructor() {
+    super();
     this.tps = conf.maxTPS;
-
-    this.mouseMoveUpdater = new Updater();
-    this.scrollUpdater = new Updater();
-    this.updater = new Updater();
-
-    this.mousemove = true;
-    this.scroll = true;
-    this.frame = 0;
   }
-  add(func, type) {
-    if (type == "mousemove") {
-      this.mouseMoveUpdater.add(func);
-    } else if (type == "scroll") {
-      this.scrollUpdater.add(func);
-    } else {
-      this.updater.add(func);
-    }
-  }
-  remove(func, type) {
-    if (type == "mousemove") {
-      this.mouseMoveUpdater.remove(func);
-    } else if (type == "scroll") {
-      this.scrollUpdater.remove(func);
-    } else {
-      this.updater.remove(func);
-    }
-  }
-  init() {
-    this.events();
-    this.update();
-  }
-  events() {
-    $(document).on("mousemove", (e) => {
-      this.mousemove = true;
-    });
-    window.scrollbar.on((state) => {
-      this.scroll = true;
-    });
-  }
-  update() {
-    if (conf.TPS_show && this.time && this.frame%10 == 0) {
+  showFramePS() {
+    if (conf.TPS_show && this.time && this.frame % 10 == 0) {
       let val = Math.round(1000 / (new Date().getTime() - this.time));
       if (val > 30) {
         console.log("tps", val);
@@ -54,31 +17,19 @@ export default class Scene {
       }
     }
     this.time = new Date().getTime();
-    this.frame ++;
+    this.frame++;
+  }
+  recall(resolve) {
     let step = 0;
-    new Promise((resolve, reject) => {
-      let checkForUpdate = () => {
-        step++;
-        step == 2 ? resolve() : 1;
-      };
-      setTimeout(() => {
-        checkForUpdate();
-      }, 1000 / this.tps);
-      requestAnimationFrame(() => {
-        //resolve()
-        checkForUpdate();
-      });
-    }).then(() => {
-      if (this.mousemove) {
-        this.mouseMoveUpdater.update();
-        this.mousemove = false;
-      }
-      if (this.scroll) {
-        this.scrollUpdater.update();
-        this.scroll = false;
-      }
-      this.updater.update();
-      this.update();
+    let checkForUpdate = () => {
+      step++;
+      step == 2 ? resolve() : 1;
+    };
+    setTimeout(() => {
+      checkForUpdate();
+    }, 1000 / this.tps);
+    requestAnimationFrame(() => {
+      checkForUpdate();
     });
   }
 }
---
+++ app/scripts/animations/controllerElems/updateController.js
@@ -0,0 +1,67 @@
+import $ from "jquery";
+import Updater from "./updater.js";
+
+export default class updateController {
+  constructor() {
+    this.mouseMoveUpdater = new Updater();
+    this.scrollUpdater = new Updater();
+    this.updater = new Updater();
+
+    this.mousemove = true;
+    this.scroll = true;
+    this.frame = 0;
+  }
+  add(func, type) {
+    if (type == "mousemove") {
+      this.mouseMoveUpdater.add(func);
+    } else if (type == "scroll") {
+      this.scrollUpdater.add(func);
+    } else {
+      this.updater.add(func);
+    }
+  }
+  remove(func, type) {
+    if (type == "mousemove") {
+      this.mouseMoveUpdater.remove(func);
+    } else if (type == "scroll") {
+      this.scrollUpdater.remove(func);
+    } else {
+      this.updater.remove(func);
+    }
+  }
+  init() {
+    this.events();
+    this.update();
+  }
+  events() {
+    $(document).on("mousemove", (e) => {
+      this.mousemove = true;
+    });
+    window.scrollbar.on((state) => {
+      this.scroll = true;
+    });
+  }
+  showFramePS() {
+    //Указать в child
+  }
+  recall(resolve) {
+    //Указать в child
+  }
+  update() {
+    this.showFramePS();
+    new Promise((resolve, reject) => {
+      this.recall(resolve);
+    }).then(() => {
+      if (this.mousemove) {
+        this.mouseMoveUpdater.update();
+        this.mousemove = false;
+      }
+      if (this.scroll) {
+        this.scrollUpdater.update();
+        this.scroll = false;
+      }
+      this.updater.update();
+      this.update();
+    });
+  }
+}
